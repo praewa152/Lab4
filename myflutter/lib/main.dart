310a7d800d35b9508c0caf889419d2d97e9e2ff1
praewa152 <ppraewa2829@gmail.com>
2022-08-22 14:59:27 +0700

    KONKAN

--- myflutter/lib/main.dart
+++ myflutter/lib/main.dart
@@ -24,38 +24,123 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("บัญชีของฉัน"),
+          title: Text("บัญชีของฉัน",style: TextStyle(fontSize: 20,color: Colors.lime,fontWeight: FontWeight.bold),),
+          title: Text(
+            "บัญชีของฉัน",
+            style: TextStyle(
+                fontSize: 20, color: Colors.lime, fontWeight: FontWeight.bold),
+          ),
         ),
-        body: Column(
-          children: [
-            Container(
-                decoration: BoxDecoration(color:Colors.yellow),
         body: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
+@@ -43,13 +47,86 @@ class _MyHomePageState extends State<MyHomePage> {
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("ยอดคงเหลือ",style: TextStyle(fontSize: 20,color: Colors.indigo,fontWeight: FontWeight.bold),),
+                    Text("11500",style: TextStyle(fontSize: 15,color: Colors.indigo,fontWeight: FontWeight.bold),)
+                    Text(
+                      "ยอดคงเหลือ",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.indigo,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "11500",
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.indigo,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.right,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+             /* Container(
+              //2
               Container(
+                padding: const EdgeInsets.all(20.0),
                 decoration: BoxDecoration(
-                    color: Colors.yellow,
+                    color: Colors.lightBlue,
                     borderRadius: BorderRadius.circular(10)),
-                height: 200,
-            ),
-            Container(
-                decoration: BoxDecoration(color:Colors.lightBlue),
+                height: 100,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "รายรับ",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "150",
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.right,
+                      ),
+                    )
+                  ],
+                ),
               ),
-              Container(
+Container(
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 100,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "รายจ่าย",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "80",
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.right,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              /* Container(
+                 padding: const EdgeInsets.all(10.0),
                 decoration: BoxDecoration(color: Colors.lightBlue,borderRadius: BorderRadius.circular(10)),
-                height: 200,
-            ),
-            Container(
-                decoration: BoxDecoration(color:Colors.lightGreen),
+                height: 100,
+                                child: Row(
+                  children: [
+                    Text("รายรับ",style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),),
+                    Text("500",style: TextStyle(fontSize: 10,color: Colors.white,fontWeight: FontWeight.bold),)
+                  ],
+                ),
               ),
-              Container(
-                decoration: BoxDecoration(color: Colors.lightGreen,borderRadius: BorderRadius.circular(10)),
-                height: 200,
-            )
-          ],
-              )
+              Container( padding: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(color: Colors.red,borderRadius: BorderRadius.circular(10)),
+                height: 100,
+                child: Row(
+                  children: [
+                    Text("รายจ่าย",style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),),
+                    Text("1150",style: TextStyle(fontSize: 10,color: Colors.white,fontWeight: FontWeight.bold),)
+                  ],
+                ),
+              ) */
             ],
           ),
         ));
