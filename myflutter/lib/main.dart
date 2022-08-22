b1b04140c0953275a190519cc6ef6e9cb9711ec0
praewa152 <ppraewa2829@gmail.com>
2022-08-22 14:42:51 +0700

    bbbf

--- myflutter/lib/main.dart
+++ myflutter/lib/main.dart
@@ -13,14 +13,30 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() =>  _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
+
 class  _MyHomePageState extends State<MyHomePage> {
 int number= 0;
+class _MyHomePageState extends State<MyHomePage> {
+  int number = 0;
+
   @override
   Widget build(BuildContext context) {
+ @override
+ Widget build(BuildContext context){
+ List<Widget> data = [];
+  data.add(Text("กดปุ่มเพื่อเพิ่มจำนวน"));
+  data.add(
+            Text(
+              number.toString(),
+              style: TextStyle(fontSize: 60),
+            ),
+            );
     return Scaffold(
         appBar: AppBar(
           title: Text("Hello Flutter"),
@@ -38,15 +54,32 @@ int number= 0;
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed:() {},
-          child: Text("add"),
           onPressed:() {
             setState(() {
-              number++;
+              number=number+10;
             });
           },
           child: Icon(Icons.add),
+      appBar: AppBar(
+        title: Text("Hello Flutter"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:data,
         ),
     );  
  }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:addNumber, 
+        child:Icon(Icons.add),
+      ),
+    );
+  }
+void addNumber() {
+  setState(() {
+    number++;
+  });
+}
 }
