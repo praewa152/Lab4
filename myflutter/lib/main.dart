3ac1953ca7a519bfb188e265dffd40ece84730a7
praewa152 <ppraewa2829@gmail.com>
2022-08-22 14:38:19 +0700

    threeoneone

--- myflutter/lib/main.dart
+++ myflutter/lib/main.dart
@@ -2,31 +2,51 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+//สร้าง widget
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
-      home: Scaffold(
+      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.lime),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() =>  _MyHomePageState();
+}
+class  _MyHomePageState extends State<MyHomePage> {
+int number= 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: Text("Hello Flutter"),
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-                Text("Hi every one"),
-                Text("Hi every one"),
-                Text("Hi every one"),
-                Text("Hi every one"),
-                Text("Hi every one"),
-                Text("Hi every one"),
-                Text("Hi every one"),
+                Text("กดปุ่มเพื่อเพิ่มจำนวน "),
+                Text(
+                number.toString(),
+                style:  TextStyle(fontSize: 60),
+                ),
             ],
           ),
         ),
-      ),
-      theme: ThemeData(primarySwatch: Colors.lime),
-    );
-  }
+        floatingActionButton: FloatingActionButton(
+          onPressed:() {},
+          child: Text("add"),
+          onPressed:() {
+            setState(() {
+              number++;
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+    );  
+ }
 }
