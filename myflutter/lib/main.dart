d9fa3b95da6a0685935eb3051f11272930a5f111
praewa152 <ppraewa2829@gmail.com>
2022-08-22 14:31:10 +0700

    threeone

--- myflutter/lib/main.dart
+++ myflutter/lib/main.dart
@@ -6,32 +6,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: "My App",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello Flutter ค่ะ"),
       title: "My App",
       home: Scaffold(
         appBar: AppBar(
           title: Text("Hello Flutter"),
         ),
         body: Center(
-          child: Text(
-            "hi",
-            style: TextStyle(fontSize: 50, color: Colors.brown),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+                Text("Hi every one"),
+                Text("Hi every one"),
+                Text("Hi every one"),
+                Text("Hi every one"),
+                Text("Hi every one"),
+                Text("Hi every one"),
+                Text("Hi every one"),
+            ],
           ),
         ),
       ),
-      body: Text("สวัสดี"),
-    ),
-    theme: ThemeData(primarySwatch: Colors.lime),
-  );
-
-
-
-
       theme: ThemeData(primarySwatch: Colors.lime),
     );
   }
-} 
 }
